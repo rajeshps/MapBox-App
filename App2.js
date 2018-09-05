e5e83ae6e760eb4e976cd49de415b5a11d2ff155
Rajesh <>
2018-09-05 17:53:08 +0530

    my location display

---
+++ App2.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Platform, View, StyleSheet } from 'react-native';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
+
+Mapbox.setAccessToken(
+  'pk.eyJ1IjoiYWRpdHlhMjAwMCIsImEiOiJjamxkcnMzdDgwZGFiM3Byb3p3dzN5bmN4In0.VXYqWSeRUDiab64DxZu_iQ'
+);
+
+const IS_ANDROID = Platform.OS === 'android';
+
+export default class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+  }
+
+  async componentWillMount() {
+    if (IS_ANDROID) {
+      const isGranted = await Mapbox.requestAndroidLocationPermissions();
+      this.setState({
+        isAndroidPermissionGranted: isGranted,
+        isFetchingAndroidPermission: false
+      });
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Mapbox.MapView
+          style={styles.container}
+          ref={map => (this.map = map)}
+          compassEnabled={true}
+          showUserLocation={true}
+          userTrackingMode={Mapbox.UserTrackingModes.followWithHeading}
+          styleURL={Mapbox.StyleURL.Dark}
+          zoomLevel={15}
+          centerCoordinate={[11.256, 43.77]}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
